8529b3cf850d3816c194a4b9cdd1c1a36b6adb55
Neville Brem <contact@nevillebrem.com>
2023-11-20 17:20:11 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { LightSwitch, storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<div>
+	<nav class="m-6 flex justify-between">
+		<a class="btn" href="/"><p class="font-[Fredoka] font-semibold h2">WG</p></a>
+		<div class="my-auto flex gap-4" />
+		<LightSwitch />
+	</nav>
+	<slot />
+	<footer class="" />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex w-full h-full">
+	<div class="flex flex-col text-center m-auto gap-2">
+		<p class="font-mono text-gray-300">Hello World, my name is</p>
+		<div class="flex flex-col gap-2">
+			<h1 class="text-6xl font-bold">Neville Brem</h1>
+			<p class="font-bold italic text-gray-300">
+				<i class="fa-solid fa-map-pin" /> Zurich, Switzerland
+			</p>
+		</div>
+		<a href="/about" class="mx-auto"
+			><button
+				class="btn py-2 px-4 text-lg font-bold mt-2 bg-primary-500 hover:bg-primary-400 shadow-md hover:shadow-lg shadow-primary-400/50 hover:shadow-primary-300/50 duration-200"
+				>Explore</button
+			></a
+		>
+	</div>
+</div>
