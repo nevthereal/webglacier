c0218613c25215187324c06c036edbc09ba5148c
Neville Brem <contact@nevillebrem.com>
2023-11-23 07:06:32 +0100

    added about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,4 +18,43 @@
 		<p class="m-auto h-96 w-96 bg-red-500">crazy svg here</p>
 	</div>
 </div>
-<div id="about">Lol</div>
+<div id="about">
+	<div class="grid grid-cols-2">
+		<div class="flex"><h2 class="m-auto h2">About Me</h2></div>
+		<img
+			src="/img/me.jpg"
+			alt="me"
+			class="m-auto md:w-[40%] rounded-xl shadow-lg shadow-black/20 hover:shadow-black/50 duration-200 ease-in-out"
+		/>
+	</div>
+	<div class="flex flex-col py-20">
+		<p class="w-[60%] mx-auto">
+			Hey, there I am a 16 years old front-end web dev. I'm based in Zurich, Switzerland. I love
+			creating websites and learning new technologies. I have a keen eye on detail and very fast
+			response times, so don't contact me if you don't need a beautiful website ASAP.
+		</p>
+	</div>
+	<div class="grid grid-cols-2 m-6">
+		<h2 class="h2 m-auto">Skills & Technologies:</h2>
+		<div class="grid grid-cols-2 gap-4 my-6">
+			<div class="card p-6">
+				<h3 class="h3">Stack 1</h3>
+				<ul class=" list-disc list-inside">
+					<li>React</li>
+					<li>TypeScript</li>
+					<li>TailwindCSS</li>
+					<li>Framer Motion</li>
+				</ul>
+			</div>
+			<div class="card p-6">
+				<h3 class="h3">Stack 2 (current)</h3>
+				<ul class=" list-disc list-inside">
+					<li>Skeleton UI</li>
+					<li>SvelteKit</li>
+					<li>TailwindCSS</li>
+					<li>TypeScript</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</div>
