c9626687137a03de8c4a3bf91bc01f7e37815fb6
Neville Brem <contact@nevillebrem.com>
2023-12-07 12:45:09 +0100

    placed text down further

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -27,13 +27,7 @@
 		<h1 class="h1 text-7xl">Projects</h1>
 		<h3 class="h3">Showcase of previous work</h3>
 	</div>
-	<p class="md:w-[50%] mb-8 mx-auto">
-		This is a selection of my previous work that went to production. You can see more of my
-		experiences and work on <a href="https://github.com/nevthereal" class="anchor" target="_blank"
-			>GitHub</a
-		>.
-	</p>
-	<div class="grid w-full md:grid-flow-col gap-6 md:gap-16">
+	<div class="grid w-full grid-cols-3 gap-16">
 		{#each _projects as project}
 			<div class="card variant-glass-primary p-8">
 				<div class="flex justify-between">
@@ -48,4 +42,10 @@
 			</div>
 		{/each}
 	</div>
+	<p class="w-[50%] my-8 mx-auto">
+		This is a selection of my previous work that went to production. You can see more of my
+		experiences and work on <a href="https://github.com/nevthereal" class="anchor" target="_blank"
+			>GitHub</a
+		>.
+	</p>
 </div>
